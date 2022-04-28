021737e5f897ca0af2118f291febb11e2157f238
Sam Huynh <samhwang2112.dev@gmail.com>
2022-04-28 11:06:03 +1000

    Add initial transformer creation call
    - Add a function to pass in ESBuildOptions that can be used to call
    ESBuild.
    - Add some util functions to get default node targets and default
    loaders for some files.

---
+++ packages/esbuild-jest-transformer/index.ts
@@ -0,0 +1,86 @@
+import * as crypto from 'node:crypto';
+import createCacheKey from '@jest/create-cache-key-function';
+import { Transformer, TransformOptions as JestOptions } from '@jest/transform';
+import {
+  transformSync,
+  transform,
+  TransformOptions as ESBuildOptions,
+  Loader,
+} from 'esbuild';
+import { getDefaultTarget, getFileExtensions, getDefaultLoader } from './utils';
+
+export type { ESBuildOptions };
+
+function buildEsbuildTransformOpts(
+  filename: string,
+  esbuildOptions?: ESBuildOptions
+): ESBuildOptions {
+  const sourcemap = esbuildOptions?.sourcemap ?? 'inline';
+  const target = esbuildOptions?.target ?? getDefaultTarget();
+
+  const extension = getFileExtensions(filename).slice(1);
+  const loader =
+    esbuildOptions?.loader ?? getDefaultLoader(extension as Loader);
+
+  return {
+    sourcemap,
+    target,
+    loader,
+    ...esbuildOptions,
+  };
+}
+
+function createTransformer(
+  esbuildTransformOptions: ESBuildOptions
+): Transformer {
+  return {
+    canInstrument: true,
+    process(content, filename, jestOpts) {
+      const esbuildOpts = buildEsbuildTransformOpts(
+        filename,
+        esbuildTransformOptions
+      );
+
+      return transformSync(content, {
+        ...esbuildOpts,
+        format: jestOpts.supportsStaticESM ? 'esm' : 'cjs',
+      });
+    },
+    processAsync(content, filename) {
+      const esbuildOpts = buildEsbuildTransformOpts(
+        filename,
+        esbuildTransformOptions
+      );
+
+      return transform(content, {
+        ...esbuildOpts,
+        format: 'esm',
+      });
+    },
+    getCacheKey(content, filename, ...opts) {
+      const esbuildOpts = buildEsbuildTransformOpts(filename);
+      const cacheKeyFunction = createCacheKey(
+        [],
+        [JSON.stringify(esbuildOpts)]
+      );
+
+      // @ts-expect-error - type overload is confused, and NewGetCacheKeyFunction is not exported from @jest/types
+      const baseCacheKey = cacheKeyFunction(content, filename, ...opts);
+
+      const options: JestOptions | undefined =
+        // @ts-expect-error - signature mismatch between Jest <27 og >=27
+        typeof opts[0] === 'string' ? opts[1] : opts[0];
+
+      return crypto
+        .createHash('md5')
+        .update(baseCacheKey)
+        .update('\0', 'utf-8')
+        .update(
+          JSON.stringify({ supportsStaticESM: options?.supportsStaticESM })
+        )
+        .digest('hex');
+    },
+  };
+}
+
+export default { createTransformer };
---
+++ packages/esbuild-jest-transformer/utils.ts
@@ -0,0 +1,31 @@
+import * as process from 'node:process';
+import * as path from 'node:path';
+import { Loader } from 'esbuild';
+
+export const DEFAULT_LOADERS: Loader[] = ['js', 'jsx', 'ts', 'tsx', 'json'];
+
+const DEFAULT_NODE_TARGETS = new Map([
+  ['12', 'es2018'],
+  ['13', 'es2019'],
+  ['14', 'es2020'],
+  ['15', 'es2021'],
+  ['16', 'es2021'],
+  ['17', 'es2022'],
+]);
+
+export function getDefaultTarget(): string {
+  return (
+    DEFAULT_NODE_TARGETS.get(process.version.match(/v(\d+)/)![1]) || 'es2018'
+  );
+}
+
+export function getFileExtensions(fileName: string): string {
+  const baseName = path.basename(fileName);
+  return path.extname(baseName).replace(/(\.[a-z0-9]+).*/i, '$1');
+}
+
+export function getDefaultLoader(extension: Loader): Loader {
+  if (DEFAULT_LOADERS.includes(extension)) return extension;
+
+  return 'text';
+}
